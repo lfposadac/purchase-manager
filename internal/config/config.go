38e59e98c97a0b0ad91e14cf4af2be7857fd64c2
lfposadac <luisposadacano12@gmail.com>
2025-07-15 17:07:55 -0500

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/lfposadac/purchase-manager/internal/domain/model"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	DB.AutoMigrate(&model.User{})
+}
